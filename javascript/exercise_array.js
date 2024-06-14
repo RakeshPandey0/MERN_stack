ed8b50198b90d2e92981d513c83d9be9cdc5ba7c
RakeshPandey0 <rp901522@gmail.com>
2024-06-14 10:35:58 +0545

    exercise

--- javascript/exercise_array.js
+++ javascript/exercise_array.js
@@ -7,7 +7,7 @@ let numbers = [1, 2, 3, 4, 5];
 
 let sum=0;
 for(let i=0; i<numbers.length; i++){
-    sum += numbers[0];
+    sum += numbers[i];
 }
 console.log(sum);
 
@@ -22,13 +22,12 @@ console.log(sum);
 //Using higher order function (reduce)
 
 sum = numbers.reduce((sum, current_value)=> sum + current_value, 0);
-
+console.log(sum);
 
 
 //Exercise 2: Filter even number
-//Using for loops
+//Using for loop
 
-let numbers_2 = [1, 2, 3, 4, 5];
 let evens = [];
 for (let i=0; i < numbers.length; i++){
     if (numbers[i]%2 === 0)
@@ -46,4 +45,46 @@ console.log(evens_for_of);
 
 
 //Using higher order functions
-let evens_filter = numbers.filter()
+let evens_filter = numbers.filter(number=>number%2===0);
+console.log(evens_filter);
+
+
+//Exercise 3: Double the values
+//Using for loop
+double_numbers = [];
+for(let i=0; i<numbers.length; i++){
+    double_numbers[i]=numbers[i]*2;
+}
+console.log(double_numbers);
+
+//Using for......of loop
+double_numbers=[];
+for(number of numbers){
+    double_numbers.push(number*2);
+}
+console.log(double_numbers);
+
+//Using higher order function map()
+double_numbers= numbers.map((number)=>number*2);
+console.log(double_numbers);
+
+//Exercise 4: Find maximum value
+//Using for loop
+max=numbers[0];
+for(let i=1; i<numbers.length; i++){
+    if(numbers[i]>max) max=numbers[i];
+}
+console.log(max);
+
+//Using for....of loop
+max=numbers[0];
+for(number of numbers){
+    if(number>max) max=number;
+}
+console.log(max);
+
+//Using higher order function reduce
+max= numbers.reduce((prev, curr)=>{
+    return curr>prev?curr:prev;
+},numbers[0])
+console.log(max);
--- javascript/exercise_class.js
+++ javascript/exercise_class.js
@@ -1,5 +1,40 @@
+//Incomplete
+
 class Book{
-    constructor(title, author,genre, pages){
-        
+
+
+    constructor(title, author,genre,pages){
+        this.title = title;
+        this.author=author;
+        this.genre=genre;
+        this.pages=pages;
     }
+
+    displayInfo(){
+        console.log(`Title:${this.title}, Author:${this.author}, Genre:${this.genre}, Pages:${this.pages}`);
+    }
+}
+
+class LibraryBook extends Book{
+    constructor(title, author, genre, pages, isAvailable){
+        super(title,author,genre,pages);
+        this.isAvailable=isAvailable;
+    }
+    displayInfo(){
+        super.displayInfo();
+        console.log(`Available:${this.isAvailable}`);
+    }
+}
+
+class Library {
+    constructor(name, location, book ){
+        this.books=[];
+        this.name=name;
+        this.location=location;
+        this.books.push(books);
+    }
+    addBook(){
+        this.books.push(book);
+    }
+    removeBook(){}
 }
