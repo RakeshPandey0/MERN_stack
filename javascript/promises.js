3518bbf9d91ff6af257f5a05af2bcebd06b19c27
RakeshPandey0 <rp901522@gmail.com>
2024-06-18 16:46:17 +0545

    api exercise

--- javascript/promises.js
+++ javascript/promises.js
@@ -7,7 +7,7 @@
 // const promiseForIntern = new Promise((resolve, reject)=>{
 //     setTimeout(()=>{
 //         resolve("Got Internship")
-//     }, 5000);  
+//     }, 5000);
 // })
 
 // console.log(promiseForIntern);
@@ -37,18 +37,35 @@
 // }).then((res)=>{console.log(res)})
 // .catch((err)=>{console.log(err)});
 
-const getUserDataByGithubUserName = (name)=>{
-        fetch(`https://api.github.com/users/${name}`).then((res)=>{
-        return res.json();
-    }).then((res)=>{return res})
+const getFollowerData = (theApi) => {
+  fetch(theApi)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+const getUserDataByGithubUserName = (name) => {
+  const result = fetch(`https://api.github.com/users/${name}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      return res;
+    })
     // .then((res)=>{console.log(res)})
-    .catch((err)=>{console.log(err)});
+    .catch((err) => {
+      return err;
+    });
+  result.then((res) => {
+    getFollowerData(res.followers_url);
+  });
+};
 
-}
+const username = prompt("Enter github username:");
+getUserDataByGithubUserName(username);
 
-const username= prompt("Enter github username:");
-
-getUserDataByGithubUserName(username).then((res)=>{
-    getUserDataByGithubUserName(res).then(console.log(res)).catch(((err)=>{console.log(err)}));
-})
-.catch((err)=>{return err;});
+// getUserDataByGithubUserName(username).then((res)=>{
+//     getUserDataByGithubUserName(res).then(console.log(res)).catch(((err)=>{console.log(err)}));
+// })
+// .catch((err)=>{return err;});
