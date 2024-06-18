fe08d3b3a321bee3f1b718d7e2b6d03aad5a934a
RakeshPandey0 <rp901522@gmail.com>
2024-06-18 17:56:19 +0545

    async and await

--- javascript/promises.js
+++ javascript/promises.js
@@ -37,35 +37,98 @@
 // }).then((res)=>{console.log(res)})
 // .catch((err)=>{console.log(err)});
 
-const getFollowerData = (theApi) => {
-  fetch(theApi)
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-};
-const getUserDataByGithubUserName = (name) => {
-  const result = fetch(`https://api.github.com/users/${name}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      return res;
-    })
-    // .then((res)=>{console.log(res)})
-    .catch((err) => {
-      return err;
-    });
-  result.then((res) => {
-    getFollowerData(res.followers_url);
-  });
-};
 
-const username = prompt("Enter github username:");
-getUserDataByGithubUserName(username);
 
-// getUserDataByGithubUserName(username).then((res)=>{
-//     getUserDataByGithubUserName(res).then(console.log(res)).catch(((err)=>{console.log(err)}));
-// })
-// .catch((err)=>{return err;});
+
+//CALLBACK HELL !!!!!!!!!!!!!!!!!
+// const getFollowerData = (theApi) => {
+//   fetch(theApi)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+// const getUserDataByGithubUserName = (name) => {
+//   const result = fetch(`https://api.github.com/users/${name}`)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((res) => {
+//       return res;
+//     })
+//     .catch((err) => {
+//       return err;
+//     });
+
+//   result.then((res) => {
+//     getFollowerData(res.followers_url);
+//   });
+// };
+
+
+//async and await
+// use await where promise object is returned
+
+const getData = async(name) => { // async function getData() {}
+    try{
+        const res = await fetch(`https://api.github.com/users/${name}`);
+        const userData = await res.json();
+        const followerRes = await fetch(userData.followers_url);
+        const follower_data= await followerRes.json();
+        console.log(follower_data);
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+// const username = prompt("Enter github username:");
+// getData(username);
+// getUserDataByGithubUserName(username);
+
+// async function getDataN(){
+//     return 'yes'; //in resolve. If throw then in rejected.
+// }
+// console.log(getDataN().then((res)=>{console.log(res)}));
+
+//https://randomuser.me/api/
+
+const fetchUserDataWithPromises = ()=>{
+    fetch('https://randomuser.me/api/').then((res)=>{
+        res.json().then((res)=>{
+            const data= res.results[0];
+            const name = data.name;
+            const location = data.location
+            const email = data.email;
+
+            console.log(`Name: ${name.first, name.last}`);
+            console.log(`location: ${location.street.name}, ${location.city}, ${location.country}`);
+            console.log(`email: ${email}`);
+            
+        })
+    })
+    
+    .catch((err)=>console.log(err))
+}
+
+fetchUserDataWithPromises();    
+
+
+//Using async and await
+
+const fetchUserDataWithPromises_2 = async()=>{
+    const res = await fetch('https://randomuser.me/api/');
+    const res_json = await res.json();
+    const data= res_json.results[0];
+    const name = data.name;
+    const location = data.location
+    const email = data.email;
+
+
+    console.log(`Name: ${name.first} ${name.last}`);
+    console.log(`location: ${location.street.name}, ${location.city}, ${location.country}`);
+    console.log(`email: ${email}`);
+}
+
+fetchUserDataWithPromises_2();
