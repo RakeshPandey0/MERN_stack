d2b5ed57acd1308bdf747afc013dd24880f090ea
RakeshPandey0 <rp901522@gmail.com>
2024-07-14 17:19:08 +0545

    mvc framework implemented

---
+++ javascript/express-todo-view/controller/task.controller.js
@@ -0,0 +1,33 @@
+const Task = require("../models/Tasks");
+
+const getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.render("index", { tasks });
+};
+
+const addTask = async (req, res) => {
+  await Task.create({ title: req.body.title });
+  res.redirect("/");
+};
+
+const deleteTask = async (req, res) => {
+  await Task.deleteOne({ _id: req.params.id });
+  res.redirect("/");
+};
+
+const editTask = async (req, res) => {
+  const taskValue = await Task.findOne({ _id: req.params.id });
+  res.send(`
+              <form method="post" id="submit-form" action="/edit/${taskValue._id}">
+              <input type="text" name="task" id="task" value="${taskValue.title}" placeholder="Search"/>
+              <input type="submit" value="Update">
+              </form>
+          `);
+};
+
+const updateTask = async (req, res) => {
+  await Task.updateOne({ _id: req.params.id }, { title: req.body.task });
+  res.redirect("/");
+};
+
+module.exports = { getTasks, addTask, deleteTask, editTask, updateTask };
--- javascript/express-todo-view/index.js
+++ javascript/express-todo-view/index.js
@@ -1,72 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const taskController = require("./controller/task.controller");
+const connectDb = require("./config/db");
+const Task = require("./models/Tasks");
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/task-management').then(() => {
-    console.log("Database Connected");
-})
-
-const taskSchema = new mongoose.Schema({
-    title: String,
-    description: String
-});
-
-const Task = mongoose.model("Task", taskSchema);
+connectDb();
+Task;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
-const todos = ["learn html", "learn js", "learn node"];
 
-
-app.get('/', async (req, res) => {
-    // let listToDisplay = "";
-    const tasks = await Task.find();
-    res.render("index", {tasks});
-});
-
-app.get("/:index", (req, res) => {
-    const index = req.params.index;
-    res.send(todos[index]);
-});
-
-
-app.post("/add", async (req, res) => {
-    await Task.create({ title: req.body.title });
-    res.redirect('/');
-
-});
-
-
-app.post("/delete/:id", async (req, res) => {
-    // const index= req.params.index;
-    // todos.splice(index, 1);
-    // res.redirect('/');
-    await Task.deleteOne({ _id: req.params.id });
-    res.redirect('/');
-});
-
-
-app.get("/edit/:id", async (req, res) => {
-
-    const taskValue = await Task.findOne({ _id: req.params.id })
-    res.send(`
-            <form method="post" id="submit-form" action="/edit/${taskValue._id}">
-            <input type="text" name="task" id="task" value="${taskValue.title}" placeholder="Search"/>
-            <input type="submit" value="Update">
-            </form>
-        `);
-
-    // res.redirect("/");
-});
-
-app.post("/edit/:id", async (req, res) => {
-    // todos[req.params.index] = req.body.task;
-    // res.redirect('/');
-    await Task.updateOne({ _id: req.params.id }, { title: req.body.task })
-    res.redirect('/');
-})
+app.get("/", taskController.getTasks);
+app.post("/add", taskController.addTask);
+app.post("/delete/:id", taskController.deleteTask);
+app.get("/edit/:id", taskController.editTask);
+app.post("/edit/:id", taskController.updateTask);
 
 app.listen(port, () => {
-    // console.log(`Example`);
-})
+  // console.log(`Example`);
+});
