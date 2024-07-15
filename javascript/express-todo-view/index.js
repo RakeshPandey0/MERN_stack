91b0e5de0bc3c6bb2c47449695e1ab3cac712887
RakeshPandey0 <rp901522@gmail.com>
2024-07-15 22:53:36 +0545

    added authentication

--- javascript/express-todo-view/index.js
+++ javascript/express-todo-view/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const taskController = require("./controller/task.controller");
+const userController = require("./controller/user.controller");
 const connectDb = require("./config/db");
 const Task = require("./models/Tasks");
+const User = require("./models/User");
 const app = express();
 const port = 3000;
 
 connectDb();
 Task;
+User;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
@@ -16,6 +19,12 @@ app.post("/add", taskController.addTask);
 app.post("/delete/:id", taskController.deleteTask);
 app.get("/edit/:id", taskController.editTask);
 app.post("/edit/:id", taskController.updateTask);
+app.get("/auth/sign-up", (req, res) => res.render("sign-up"));
+app.get("/auth/sign-in", (req, res) =>
+  res.render("sign-in", { message: null })
+);
+app.post("/auth/sign-up", userController.signUpPost);
+app.post("/auth/sign-in", userController.signIn);
 
 app.listen(port, () => {
   // console.log(`Example`);
